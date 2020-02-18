93ec3bd14977202c2b373fc59391cca4b837e909
Sarunas Medeikis <sarunasmedeikis@gmail.com>
2020-02-18 22:50:55 +0200

    Updating/Editing an item

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ let mongodb = require("mongodb");
 let app = express();
 let db;
 
+app.use(express.static("public"));
+
 let connectionString="mongodb+srv://todoAppUser:todoAppUser@cluster0-bdgsq.mongodb.net/TodoApp?retryWrites=true&w=majority";
 
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true} , function(err, client){
@@ -14,6 +16,7 @@ mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: tr
   app.listen(3000);
 });
 
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get("/", function(req, res){
@@ -45,13 +48,15 @@ app.get("/", function(req, res){
           return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
             <span class="item-text">${item.text}</span>
             <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
               <button class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
           </li>`
         }).join("")}
       </ul>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="/browser.js"></script>
   </body>
   </html>`);
   });
@@ -62,4 +67,10 @@ app.post("/create-item", function(req, res){
   db.collection("items").insertOne({text: req.body.item}, function(){
       res.redirect("/");
   })
+});
+
+app.post("/update-item", function(req, res){
+  db.collection("items").findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text:req.body.text}}, function(){
+    res.send("Success");
+  });
 });
